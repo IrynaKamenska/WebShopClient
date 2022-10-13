ee40717d28c62ba430b5906bb1850bbae6df761e
Iryna <pshonka@gmail.com>
2022-10-13 20:20:57 +0200

    add project files

--- src/main/java/com/example/webshopclient/WebShopClientApplication.java
+++ src/main/java/com/example/webshopclient/WebShopClientApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebShopClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebShopClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebShopClientApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/webshopclient/WebShopClientApplicationTests.java
+++ src/test/java/com/example/webshopclient/WebShopClientApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WebShopClientApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
